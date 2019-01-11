c423d725cd7909318f91f43adce0b1f848764ec5
yotam elor <yotam.elor@gmail.com>
2019-01-11 23:26:05 +0200

    initial build order

--- Source/Macro/Strategy.cpp
+++ Source/Macro/Strategy.cpp
@@ -5,27 +5,43 @@
 
 
 using namespace BWAPI;
+using namespace BWAPI::UnitTypes;
 using namespace std;
 
 
 namespace Amorphis {
 
 
+	Strategy::Strategy()
+	{
+		// initial units
+		m_buildOrder.push_back(make_pair(Zerg_Drone, 4));
+		m_buildOrder.push_back(make_pair(Zerg_Overlord, 1));
+		// build order
+		m_buildOrder.push_back(make_pair(Zerg_Drone, 5));
+		m_buildOrder.push_back(make_pair(Zerg_Overlord, 1));
+		m_buildOrder.push_back(make_pair(Zerg_Spawning_Pool, 1));
+		m_buildOrder.push_back(make_pair(Zerg_Extractor, 1));
+		m_buildOrder.push_back(make_pair(Zerg_Drone, 1));
+		m_buildOrder.push_back(make_pair(Zerg_Zergling, 6));
+	}
+
+
 	void Strategy::onFrame()
 	{
-		if (UM->unitsCounter().getCounter(UnitTypes::Zerg_Drone) > 8) {
-			if (UM->unitsCounter().getCounter(UnitTypes::Zerg_Spawning_Pool) == 0) {
-				m_nextToBuild = UnitTypes::Zerg_Spawning_Pool;
+		m_nextToBuild = None;
+		std::map<BWAPI::UnitType, int> requiredUnitCount;
+		for (int i = 0; i < (int)m_buildOrder.size(); i++) {
+			if (requiredUnitCount.count(m_buildOrder[i].first) == 0) {
+				requiredUnitCount[m_buildOrder[i].first] = m_buildOrder[i].second;
 			}
 			else {
-				m_nextToBuild = UnitTypes::Zerg_Zergling;
+				requiredUnitCount[m_buildOrder[i].first] += m_buildOrder[i].second;
 			}
-		}
-		else if (UM->unitsCounter().getCounter(UnitTypes::Zerg_Drone) == 8 && UM->unitsCounter().getCounter(UnitTypes::Zerg_Overlord) == 1) {
-			m_nextToBuild = UnitTypes::Zerg_Overlord;
-		} 
-		else {
-			m_nextToBuild = UnitTypes::Zerg_Drone;
+			if (UM->unitsCounter().getCounter(m_buildOrder[i].first) < requiredUnitCount[m_buildOrder[i].first]) {
+				m_nextToBuild = m_buildOrder[i].first;
+				break;
+			}
 		}
 	}
 
--- Source/Macro/Strategy.hpp
+++ Source/Macro/Strategy.hpp
@@ -9,8 +9,9 @@ namespace Amorphis {
 	class Strategy {
 		std::map<BWAPI::UnitType, int> m_unitsCounter;
 		BWAPI::UnitType m_nextToBuild = NULL;
+		std::vector< std::pair<BWAPI::UnitType, int> > m_buildOrder;
 	public:
-		Strategy() {}
+		Strategy();
 		void onFrame();
 		BWAPI::UnitType nextToBuild() const { return m_nextToBuild; }
 		void draw() const;
